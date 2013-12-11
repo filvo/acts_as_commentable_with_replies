fa49815caef8d9fce4c5a62ba5fc6edef1300485
Akash Kamboj <akashkamboj@Akashs-Mac-Pro.local>
2013-12-11 13:39:13 +0530

    Updated for Rails 4

--- lib/generators/acts_as_commentable_with_replies/migration/templates/active_record/comment.rb
+++ lib/generators/acts_as_commentable_with_replies/migration/templates/active_record/comment.rb
@@ -1,15 +1,15 @@
 class Comment < ::ActiveRecord::Base
+  
+  # settings
   acts_as_nested_set
 
-  attr_accessible :commentable_id, :commentable_type,
-                  :commenter_id, :commenter_type,
-                  :commentable, :commenter,
-                  :message
+  # relationships
+  belongs_to :commentable, polymorphic: true, touch: true
+  belongs_to :commenter, polymorphic: true, touch: true
 
-  belongs_to :commentable, :polymorphic => true
-  belongs_to :commenter, :polymorphic => true
-
+  # validations
   validates_presence_of :commentable_id
   validates_presence_of :commenter_id
   validates_presence_of :message
+  
 end
